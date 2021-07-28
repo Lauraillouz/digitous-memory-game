42bd9a727f5c22086c3df33a07c51d84e6d6ee14
yangchen <gyachungriom@gmail.com>
2021-07-28 11:37:58 +0200

    Style added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from "react";
 import Button from "./component/Button";
 import GameOver from "./component/GameOver";
 import Grid from "./component/Grid";
+import Css from './App.css';
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <h1>Memory Card</h1>
+      <div className= "container">
+        <div className="head">
+          <h1>Memory Card</h1>
+          <span>by Classy glassy</span>
+        </div>
         <Button />
       </div>
     );
