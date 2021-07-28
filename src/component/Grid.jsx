57be715d384a93a7a39117532adc35be45b7d83f
mridha-emran <bari.lala490@gmail.com>
2021-07-28 15:22:27 +0200

    'chang

---
+++ src/component/Grid.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+class Grid extends React.Component {
+
+  many (){
+    {this.props.images.map((image) => {
+      return <img src={image.URL} alt={image.name} key={image.id}></img>
+    })}
+  }
+  render() {
+    return (
+      <div>
+        many ()
+        
+      </div>
+    );
+  }
+}
+
+export default Grid;
